e3e6004de781d6d9b8b57ae2cb81abc7e2084e81
Benard A. Adjei <benardadjei95@gmail.com>
2025-06-30 00:12:05 +0000

    Initial commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+# Perimeter 
+from math import sqrt
+
+
+a = float(input("Enter the value of a: "))
+b = float(input("Enter the value of b: "))
+
+c = sqrt(a**2 + b**2)
+print(f"The area of the triangle is {round(c,3)} cm^2")
---
+++ main.py
@@ -0,0 +1,11 @@
+from mean_var_std import calculate  # Import your function from the other file
+
+# Create a list with 9 numbers
+input_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+# Call the function with the list
+result = calculate(input_list)
+
+# Print the result in a readable way
+from pprint import pprint
+pprint(result)
